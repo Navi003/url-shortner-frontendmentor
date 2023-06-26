408c4f399a72c562a553f7706c6d9adc71ff9902
Navjot Dhiman <dhimannavjot1@gmail.com>
2023-06-26 21:47:51 +0200

    changed  error.message to error

--- app/api/route.ts
+++ app/api/route.ts
@@ -19,7 +19,7 @@ export const POST = async (request: NextRequest, response: NextResponse) => {
       statusText: "Sucess",
     });
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
     return new Response("Failed to Fetch", { status: 404 });
   }
 };
